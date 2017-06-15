fc8ae76bbe71e1a7c0d42fffc406dc6e76c07ff3
muerschi <ninamuerschberger@web.de>
2017-06-15 22:14:53 +0200

    Bug Fixed

---
+++ app/src/main/java/com/example/tiffany/eventsproject/Helper/Global.java
@@ -0,0 +1,10 @@
+package com.example.tiffany.eventsproject.Helper;
+
+/**
+ * Created by Ich on 15.06.2017.
+ */
+
+public class Global {
+
+    public static String WEBSERVER_IP = "http://141.19.96.199:8080/PMAWebServer";
+}
